e58dcb0f580f7da03ffb26553ba77f941d12d2df
ookamilc@aliyun.com <17425ookami>
2021-12-30 20:45:05 +0800

    完成组件代码生成功能.

--- Assets/LuaScripts/Game/UI/Login/LoginView.lua
+++ Assets/LuaScripts/Game/UI/Login/LoginView.lua
@@ -1,19 +1,24 @@
 ---@class LoginView : BasePanel
----
-local LoginView = BaseClass(BasePanel)
+local LoginView = require("Game/UI/Login/LoginViewGenerate")
 
-function LoginView:Constructor()
-    self.conf = {
-        prefabPath = "Assets/AssetBundleRes/UI/LoginPanel.prefab";
-        root = "High";
-    }
-
+function LoginView:OnLoad()
+    self:InitEvent()
 end
 
-function LoginView:InitUIComponent(root)
-    self.objEnter = root:Find("Obj_Enter").gameObject
-    self.enterImage = root:Find("Obj_Enter/Image"):GetComponent("Image")
-    self.btnLogin = root:Find("Btn_Login"):GetComponent("Button")
+function LoginView:Init(controller)
+    ---@type LoginController
+    self.ctrl = controller
+end
+
+function LoginView:InitEvent()
+    
+    self.btnLogin.onClick:AddListener(function()
+        self:ClickLoginButton()
+    end)
+end
+
+function LoginView:ClickLoginButton()
+    self.ctrl:StartLogin("123","123")
 end
 
 return LoginView
